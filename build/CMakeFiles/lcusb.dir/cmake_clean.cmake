9b1365ade4096d49604807e24f75a1ba9a4885eb
U-MCHWE001\vkuzmenko <vkuzmenko@DESK-VKU.MCHWE001.local>
2019-08-22 11:59:52 +0200

    dynamic library

--- build/CMakeFiles/lcusb.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "liblcusb.a"
-  "liblcusb.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
-  include(CMakeFiles/lcusb.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/objlib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/objlib.dir/src/LCUsb.cpp.obj"
-  "CMakeFiles/objlib.dir/src/hid.c.obj"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
-  include(CMakeFiles/objlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
