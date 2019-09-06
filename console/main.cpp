9e5258c35e7fb18eaf102c190982d2f0a02e4850
arsenal007 <vasylkuzmenko85@gmail.com>
2019-09-06 08:05:15 +0200

    TCLAP

--- console/main.cpp
+++ console/main.cpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 #include <vector>
 #include <LCUsb.hpp>
+#include <tclap/CmdLine.h>
 
 inline bool yesno( const std::string& prompt )
 {
@@ -188,90 +189,122 @@ struct TCalibrate
 int main( int argc, char** argv )
 {
   TExecute lc;
-  auto res = lc.init( []( auto t ) {
-    {
-      auto refC = std::get<0>( t );
-      auto refL = std::get<1>( t );
-      std::cout << "C: refC: " << refC << "pF, refL: " << refL << "nH" << std::endl;
-    }
-    {
-      auto refC = std::get<2>( t );
-      auto refL = std::get<3>( t );
-      std::cout << "L: refC: " << refC << "pF, refL: " << refL << "nH" << std::endl;
-    }
-  } );
-  std::this_thread::sleep_for( std::chrono::seconds( 2 ) );
-  InputParser input{ argc, argv };
-  if ( input.cmdOptionExists( "-c" ) )
+  try
   {
-    lc.C_measurments( []( auto Capicatance ) {
-      std::string p{ " pF" };
-      if ( Capicatance > 1000.0 )
+    // Define the command line object.
+    TCLAP::CmdLine cmd( "USB LC Meter, simple demonstration console application", ' ', "0.1", true );
+
+    // Define a value argument and add it to the command line.
+    TCLAP::ValueArg<uint16_t> ref( "r", "reference", "calibration reference value of capicator or inductance", false,
+                                   1000, "uint16_t" );
+
+    cmd.add( ref );
+
+    TCLAP::ValueArg<double> tolerance_( "t", "tolerance", "treshold level tolerance", false, 0.000006, "double" );
+
+    cmd.add( tolerance_ );
+
+    // Define a switch and add it to the command line.
+    TCLAP::SwitchArg capicatance( "c", "capicatance", "measure capicatance mode", false );
+    cmd.add( capicatance );
+
+    TCLAP::SwitchArg inductance( "l", "inductance", "measure inductance mode", false );
+    cmd.add( inductance );
+
+    // Parse the args.
+    cmd.parse( argc, argv );
+
+    // Get the value parsed by each arg.
+
+    auto res = lc.init( []( auto t ) {
       {
-        p = " nF";
-        Capicatance /= 1000.0;
+        auto refC = std::get<0>( t );
+        auto refL = std::get<1>( t );
+        std::cout << "C: refC: " << refC << "pF, refL: " << refL << "nH" << std::endl;
+      }
+      {
+        auto refC = std::get<2>( t );
+        auto refL = std::get<3>( t );
+        std::cout << "L: refC: " << refC << "pF, refL: " << refL << "nH" << std::endl;
+      }
+    } );
+    std::this_thread::sleep_for( std::chrono::seconds( 2 ) );
+    //InputParser input{ argc, argv };
+    if ( capicatance.getValue() )
+    {
+      lc.C_measurments( []( auto Capicatance ) {
+        std::string p{ " pF" };
         if ( Capicatance > 1000.0 )
         {
-          p = " mF";
+          p = " nF";
           Capicatance /= 1000.0;
+          if ( Capicatance > 1000.0 )
+          {
+            p = " mF";
+            Capicatance /= 1000.0;
+          }
         }
-      }
 
-      std::cout.setf( std::ios::fixed );
-      std::cout.setf( std::ios::showpoint );
-      std::cout.precision( 2 );
-      std::cout << "\r";
-      for ( size_t j = 0; j < 40; j++ )
-        std::cout << " ";
-      std::cout.flush();
+        std::cout.setf( std::ios::fixed );
+        std::cout.setf( std::ios::showpoint );
+        std::cout.precision( 2 );
+        std::cout << "\r";
+        for ( size_t j = 0; j < 40; j++ )
+          std::cout << " ";
+        std::cout.flush();
 
-      std::cout << "\rCapicatance: " << Capicatance << p;
-      std::cout.flush();
-    } );
-  }
-  else if ( input.cmdOptionExists( "-l" ) )
-  {
-    lc.L_measurments( []( auto Inducatance ) {
-      std::string p{ " nH" };
-      if ( Inducatance > 1000.0 )
-      {
-        p = " mkH";
-        Inducatance /= 1000.0;
+        std::cout << "\rCapicatance: " << Capicatance << p;
+        std::cout.flush();
+      } );
+    }
+    else if ( false )
+    {
+      lc.L_measurments( []( auto Inducatance ) {
+        std::string p{ " nH" };
         if ( Inducatance > 1000.0 )
         {
-          p = " mH";
+          p = " mkH";
           Inducatance /= 1000.0;
+          if ( Inducatance > 1000.0 )
+          {
+            p = " mH";
+            Inducatance /= 1000.0;
+          }
         }
-      }
 
-      std::cout.setf( std::ios::fixed );
-      std::cout.setf( std::ios::showpoint );
-      std::cout.precision( 2 );
-      std::cout << "\r";
-      for ( size_t j = 0; j < 40; j++ )
-        std::cout << " ";
-      std::cout.flush();
+        std::cout.setf( std::ios::fixed );
+        std::cout.setf( std::ios::showpoint );
+        std::cout.precision( 2 );
+        std::cout << "\r";
+        for ( size_t j = 0; j < 40; j++ )
+          std::cout << " ";
+        std::cout.flush();
 
-      std::cout << "\rInducatance: " << Inducatance << p;
-      std::cout.flush();
-    } );
+        std::cout << "\rInducatance: " << Inducatance << p;
+        std::cout.flush();
+      } );
+    }
+    else if ( false )
+    {
+      lc.C_calibration( double{ 1000.0 }, double{ 0.000006 }, TCalibrate{}, []() -> bool {
+        std::cout << std::endl << "press ENTER to save..." << std::endl;
+
+        if ( std::cin.get() != '\n' )
+        {
+          return ( true );
+        }
+        else
+        {
+          return ( false );
+        }
+      } );
+    }
   }
-  else if ( input.cmdOptionExists( "-nc" ) )
+  catch ( TCLAP::ArgException& e )
   {
-    lc.C_calibration( double{ 1000.0 }, double{ 0.000006 }, TCalibrate{}, []() -> bool {
-      std::cout << std::endl << "press ENTER to save..." << std::endl;
-
-      if ( std::cin.get() != '\n' )
-      {
-        return ( true );
-      }
-      else
-      {
-        return ( false );
-      }
-    } );
+    std::cout << "ERROR: " << e.error() << " " << e.argId() << std::endl;
   }
-  while ( res )
+  while ( true )
     std::this_thread::sleep_for( std::chrono::milliseconds( 2000 ) );
   lc.deinit();
 }
